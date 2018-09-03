b390094265b44444cb27abedd73e753f5ae521fa
Developer <comps381f@ouhk.edu.hk>
2018-09-03 11:56:30 +0800

    added my API key to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ cors = require('cors');
 
 var express = require('express');
 var app = express();
-var APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
+var APIKEY = "e752edfdfdcaa199ae4552fde771b20e";  //signup at api.openweathermap.org and obtain an API Key
 
 var options = {
     host: 'api.openweathermap.org',
